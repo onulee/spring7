59bf58a4af0676fa41e0622a381b917a440ad55c
onulee <onulee@naver.com>
2026-02-12 18:19:01 +0900

    쿠키,섹션 추가

--- s0212_01/src/main/java/com/java/controller/MemberContoller.java
+++ s0212_01/src/main/java/com/java/controller/MemberContoller.java
@@ -1,5 +1,7 @@
 package com.java.controller;
 
+import java.net.http.HttpResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.java.dto.MemberDto;
 import com.java.service.MemberService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -59,16 +63,28 @@ public class MemberContoller {
 		return "login";
 	}
 	
-	// 로그인확인
+	// 로그인확인 - Session,Cookie
 	@PostMapping("/member/login")
-	public String login(MemberDto mdto) {
+	public String login(MemberDto mdto,String saveId,
+			HttpServletResponse response) {
 		System.out.println("id,pw : "+mdto.getId()+","+mdto.getPw());
+		System.out.println("saveId : "+saveId);
+		//아이디저장 - cookie
+		Cookie cookie = new Cookie("cook_id", mdto.getId());
+		if(saveId !=null) {
+			cookie.setMaxAge(60*60*24*30); //60초X60분X24시간X30일
+		}else {
+			cookie.setMaxAge(0);
+		}
+		response.addCookie(cookie);
+		
 		//로그인 1개 객체가져오기
 		MemberDto memberDto = memberService.selectLogin(mdto);
 		if(memberDto != null) {
 			System.out.println("id,pw 존재함");
 			session.setAttribute("session_id", memberDto.getId());
 			session.setAttribute("session_name", memberDto.getName());
+			
 			return "redirect:/?flag=1";
 		}else {
 			System.out.println("id,pw 일치하지 않음.");
