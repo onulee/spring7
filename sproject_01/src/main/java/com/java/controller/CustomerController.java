e0710a53308a3db5d97a66a9f5ee93c86993292e
onulee <onulee@naver.com>
2026-03-18 18:23:55 +0900

    react : 리액트 게시판 연결

--- sproject_01/src/main/java/com/java/controller/CustomerController.java
+++ sproject_01/src/main/java/com/java/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.java.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.java.dto.BoardDto;
 import com.java.service.CustomerService;
@@ -27,6 +30,20 @@ public class CustomerController {
 		Map<String, Object> map = customerService.findAll(page,category,search); 
 		model.addAttribute("map",map);
 		return "customer/customer";
+	} 
+	
+	//01-01.게시판리스트 : react 게시판 연결
+	@CrossOrigin(origins = "http://localhost:3000")
+	@ResponseBody
+	@GetMapping("/customer/customerJson")
+	public Map<String, Object> customerJson(
+			@RequestParam(name="page",defaultValue = "1") int page,
+			@RequestParam(name="category",required = false) String category,
+			@RequestParam(name="search",required = false) String search
+			) {
+		Map<String, Object> map = customerService.findAll(page,category,search); 
+		List<BoardDto> list = (List<BoardDto>) map.get("list");
+		return map;
 	} 
 	
 	//02.게시글쓰기
